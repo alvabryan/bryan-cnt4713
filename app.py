5fbf61c6e3dac8d0a1e8ba77bd103f07449466dd
gelsonjr <gelsonjr@gmail.com>
2022-07-15 09:03:23 -0400

    new repository

---
+++ app.py
@@ -0,0 +1,62 @@
+import json
+import os
+from urllib.parse import quote_plus, urlencode
+from authlib.integrations.flask_client import OAuth
+from flask import Flask, redirect, render_template, session, url_for
+
+
+app = Flask(__name__)
+
+APP_SECRET_KEY = os.environ.get("APP_SECRET_KEY")
+AUTH0_CLIENT_ID = os.environ.get("AUTH0_CLIENT_ID")
+AUTH0_CLIENT_SECRET = os.environ.get("AUTH0_CLIENT_SECRET")
+AUTH0_DOMAIN = os.environ.get("AUTH0_DOMAIN")
+
+app.secret_key = APP_SECRET_KEY
+
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=AUTH0_CLIENT_ID,
+    client_secret=AUTH0_CLIENT_SECRET,
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{AUTH0_DOMAIN}/.well-known/openid-configuration'
+)
+
+@app.route("/login")
+def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        "https://" + AUTH0_DOMAIN
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("home", _external=True),
+                "client_id": AUTH0_CLIENT_ID,
+            },
+            quote_via=quote_plus,
+        )
+    )
+
+@app.route("/")
+def home():
+    return render_template("home.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=os.environ.get("PORT", 5000))
