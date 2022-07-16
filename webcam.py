ae9fd5138d2a090c883642ed4e867f4e5b80914b
gelsonjr <gelsonjr@gmail.com>
2022-07-16 16:27:28 -0400

    again

--- webcam.py
+++ webcam.py
@@ -18,7 +18,7 @@ def gen_frames():
                     b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
-@app.route('/video_feed')
+@app.route('/live_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
